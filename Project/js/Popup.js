af03d00056288bcbf5249a64ff8e2c48de586a1d
wdyt0228 <132254396+wdyt0228@users.noreply.github.com>
2023-05-18 13:05:31 +0900

    Add files via upload

--- Project/js/Popup.js
+++ Project/js/Popup.js
@@ -1,81 +1,48 @@
-
+// $(function() {
+//     for (var i = 1; i <= 13; i++) {
+//       $("#fadeInBtn" + i).on("click", function() {
+//         $("#divBox" + i).fadeIn();
+//       });
+//     }
+// });
 $(function() {
-    $("#fadeInBtn2").on("click", function() {
-        $("#divBox2").fadeIn();
-    });
-    $("#fadeOutBtn2").on("click", function() {
-        $("#divBox2").fadeOut();
-    });
     $("#fadeInBtn1").on("click", function() {
         $("#divBox1").fadeIn();
     });
-    $("#fadeOutBtn1").on("click", function() {
-        $("#divBox1").fadeOut();
+    $("#fadeInBtn2").on("click", function() {
+        $("#divBox2").fadeIn();
     });
     $("#fadeInBtn3").on("click", function() {
         $("#divBox3").fadeIn();
     });
-    $("#fadeOutBtn3").on("click", function() {
-        $("#divBox3").fadeOut();
-    });
     $("#fadeInBtn4").on("click", function() {
         $("#divBox4").fadeIn();
-    });
-    $("#fadeOutBtn4").on("click", function() {
-        $("#divBox4").fadeOut();
     });
     $("#fadeInBtn5").on("click", function() {
         $("#divBox5").fadeIn();
     });
-    $("#fadeOutBtn5").on("click", function() {
-        $("#divBox5").fadeOut();
-    });
     $("#fadeInBtn6").on("click", function() {
         $("#divBox6").fadeIn();
-    });
-    $("#fadeOutBtn6").on("click", function() {
-        $("#divBox6").fadeOut();
     });
     $("#fadeInBtn7").on("click", function() {
         $("#divBox7").fadeIn();
     });
-    $("#fadeOutBtn7").on("click", function() {
-        $("#divBox7").fadeOut();
-    });
     $("#fadeInBtn8").on("click", function() {
         $("#divBox8").fadeIn();
-    });
-    $("#fadeOutBtn8").on("click", function() {
-        $("#divBox8").fadeOut();
     });
     $("#fadeInBtn9").on("click", function() {
         $("#divBox9").fadeIn();
     });
-    $("#fadeOutBtn9").on("click", function() {
-        $("#divBox9").fadeOut();
-    });
     $("#fadeInBtn10").on("click", function() {
         $("#divBox10").fadeIn();
-    });
-    $("#fadeOutBtn10").on("click", function() {
-        $("#divBox10").fadeOut();
     });
     $("#fadeInBtn11").on("click", function() {
         $("#divBox11").fadeIn();
     });
-    $("#fadeOutBtn11").on("click", function() {
-        $("#divBox11").fadeOut();
-    });
     $("#fadeInBtn12").on("click", function() {
         $("#divBox12").fadeIn();
     });
-    $("#fadeOutBtn12").on("click", function() {
-        $("#divBox12").fadeOut();
-    });
     $("#fadeInBtn13").on("click", function() {
         $("#divBox13").fadeIn();
-    });
-    $("#fadeOutBtn13").on("click", function() {
-        $("#divBox13").fadeOut();
     });
 });
